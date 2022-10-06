32503ef3e407c66d4e89842ae40aa222b715459e
Carlos Eduardo <cadu.010808@gmail.com>
2022-10-05 22:09:45 -0300

    videos mostrados direto da API

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Box, CardContent, CardMedia, Typography } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
 
-const ChannelCard = () => {
+import { demoProfilePicture } from '../utils/constants';
+
+const ChannelCard = ({ channelDetail }) => {
   return (
-    <div>ChannelCard</div>
+    <Box 
+       sx={{
+        boxShadow: 'none',
+        borderRadius: '20px'
+       }}>
+         <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+            <CardContent sx={{ display: 'flex', flexDirection: 'column',
+           justifyContent: 'center', alignItems: 'center', color: '#fff'}}>
+              <CardMedia
+                  image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
+                  alt={channelDetail?.snippet?.title}
+                  sx={{ borderRadius: '50%', height: '180px', width: '180px',
+                   mb: 2, border: '1px solid #e3e3e3'}}
+                    />
+                    <Typography variant='h6'>
+                        {channelDetail?.snnipet?.title}
+                        <CheckCircle sx={{ fontSize: 14, color: 'gray', ml: '5px'}} />
+                    </Typography>
+
+            </CardContent>
+         </Link> 
+    </Box>
   )
 }
 
